fb5fd680a6179450f07a2f91f9a3364fa0e12934
Aditya Singh <adising@microsoft.com>
2019-07-13 12:55:27 +0530

    Navbar and component layout updated.

--- HouseParty.Client/ClientApp/src/components/app.module.ts
+++ HouseParty.Client/ClientApp/src/components/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
